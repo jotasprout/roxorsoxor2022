1de85a8b82f10d46f7fc485fd4ffdd4ed5f49df4
Jay Sprout <jotasprout@gmail.com>
2021-02-23 18:13:27 -0500

    Debugging Navbar

--- poprock/page_pieces/stylesAndScripts.php
+++ poprock/page_pieces/stylesAndScripts.php
@@ -2,7 +2,10 @@
 
 $stylesAndSuch = "
     <meta charset='UTF-8'> 
+    <!--
     <link rel='stylesheet' type='text/css' href='https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css'>
+    -->
+    <link rel='stylesheet' type='text/css' href='https://www.roxorsoxor.com/poprock/css/slate/bootstrap.min.css'>
     <script src='https://code.jquery.com/jquery-3.3.1.min.js'></script>
     <script src='https://code.jquery.com/ui/1.12.1/jquery-ui.js'></script>
     <link rel='stylesheet' href='https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css'>
@@ -18,13 +21,14 @@ $stylesAndSuch = "
     <meta name='viewport' content='user-scalable=no, width=device-width, initial-scale=1' />
     <meta name='apple-mobile-web-app-capable' content='yes'>
     <meta name='mobile-web-app-capable' content='yes'>    
-<!--
+
+    <!--
     <script src='https://www.roxorsoxor.com/js/pouchdb7.min.js'></script>
     <script src='js/app.js'></script>
-        <script src='https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js'></script>
-    <script src='https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js'></script>
-    <link rel='stylesheet' href='https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css'>
--->
+    -->
+    <script src='https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js'></script>
+    <script src='https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js'></script>
+    <link rel='stylesheet' href='https://cdn.datatables.net/1.10.23/css/dataTables.bootstrap4.min.css'>
 ";
 
 $scriptsAndSuch = "
