00d8e87a25ee632e7ff223a980126f7ae984e3c0
Jay Sprout <jotasprout@gmail.com>
2021-04-01 12:01:05 -0400

    Update filters in JS and ID-index

--- js/filters.js
+++ js/filters.js
@@ -2,14 +2,9 @@
 
 $(document).ready(function(){
     $('li').on('click', function() {
-        // const $software = $('#'+$(this).data('content-id'));
         $software = $(this).data('content-id');
-        $notSoftware = $('div').not('.'+$software);
-           // $('.'+$(this).data('content-id'))
-           $notSoftware.fadeToggle();
-
-       // $('.'+$(this).data('content-id')).fadeToggle();
-       // $software.fadeToggle();
-       console.log($software);
+        $notSoftware = $('div#container2 > div').not('.'+$software);
+        $notSoftware.fadeToggle();
+        //console.log($software);
     });
 });
